ce55c2024c8d076aaf9eb582467e9fd562486dee
kksty <2731502523@qq.com>
2024-04-19 15:27:37 +0800

    完成到计算净力

---
+++ proj0/Planet.java
@@ -0,0 +1,72 @@
+
+public class Planet  {
+    public double xxPos; //当前x位置
+    public double yyPos; //当前y位置
+    public double xxVel; //当前x方向速度
+    public double yyVel; //当前y方向速度
+    public double mass; //质量
+    public String imgFileName;
+    static final double G = 6.67E-11;
+
+    public Planet(double xP, double yP, double xV, double yV, double m, String img){
+       this.xxPos = xP;
+       this.yyPos = yP;
+       this.xxVel = xV;
+       this.yyVel = yV;
+       this.mass = m;
+       this.imgFileName = img;
+    }
+    public Planet(Planet p){
+        this.xxPos = p.xxPos;
+        this.yyPos = p.yyPos;
+        this.xxVel = p.xxVel;
+        this.yyVel = p.yyVel;
+        this.mass = p.mass;
+        this.imgFileName = p.imgFileName;
+    }
+
+    //计算两个天体之间的距离
+     public double calcDistance(Planet p){
+        double dx = p.xxPos - this.xxPos;
+        double dy = p.yyPos - this.yyPos;
+        double rSquared = dx * dx + dy * dy;
+        return Math.sqrt(rSquared);
+    }
+
+    //计算给定物体对该物体施加的力。
+    public double calcForceExertedBy(Planet p){
+        double F = ((G * p.mass * this.mass ) / (calcDistance(p) * calcDistance(p)));
+        return F;
+    }
+
+    //分别计算 X Y 方向上施加的力
+    public double calcForceExertedByX(Planet p){
+        double Fx = ((calcForceExertedBy(p) * (p.xxPos - this.xxPos)) / calcDistance(p));
+        return Fx;
+    }
+    public double calcForceExertedByY(Planet p){
+        double Fy = ((calcForceExertedBy(p) * (p.yyPos - this.yyPos)) / calcDistance(p));
+        return Fy;
+    }
+
+    //接收一个 Planet 数组，并计算该数组中所有 Planet 对当前 Planet 施加的净 X 和净 Y 力
+    public double calcNetForceExertedByX(Planet[] allPlanets){
+        double Fnx = 0;
+        for (int i = 0; i < allPlanets.length; i++) {
+            if (!this.equals(allPlanets[i])){
+                Fnx += calcForceExertedByX(allPlanets[i]);
+            }
+        }
+        return Fnx;
+    }
+    public double calcNetForceExertedByY(Planet[] allPlanets){
+        double Fny = 0;
+        for (int i = 0; i < allPlanets.length; i++) {
+            if (!this.equals(allPlanets[i])){
+                Fny += calcForceExertedByY(allPlanets[i]);
+            }
+        }
+        return Fny;
+    }
+
+}
