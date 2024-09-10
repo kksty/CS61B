c5167a1c32da1730effa186e2f454d16985a6d8a
XeeKee <2731502523@qq.com>
2024-09-10 13:54:35 +0800

    hw3

--- hw3/hw3/hash/SimpleOomage.java
+++ hw3/hw3/hash/SimpleOomage.java
@@ -35,10 +35,7 @@ public class SimpleOomage implements Oomage {
         if (!USE_PERFECT_HASH) {
             return red + green + blue;
         } else {
-            red = red / 5;
-            green = green / 5;
-            blue = blue / 5;
-            return 32 * red + 56 * green + 91 * blue;
+            return 31 * (red / 5) + 37 * (green / 5) + 41 * (blue / 5);
         }
     }
 
